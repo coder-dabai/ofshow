48e6c5ccab227230e60083be642f87e140792cfb
coder-dabai <631389205@qq.com>
2024-10-15 22:13:58 +0800

    feat: 添加vuedragble增加拖拽效果

--- apps/FormDesign/src/components/Source/SourceContainer.tsx
+++ apps/FormDesign/src/components/Source/SourceContainer.tsx
@@ -1,6 +1,7 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import type { Source as SourceType } from './index.d';
 import { getImgUrl } from '@form/utils.ts';
+import draggable from 'vuedraggable';
 
 export default defineComponent({
   name: 'SourceContainer',
@@ -14,21 +15,30 @@ export default defineComponent({
       type: String,
     },
   },
+  components: { draggable },
   setup(props) {
+    const dataList = ref<any[]>(props.sourceList);
     const renderSouceList = () => {
       return (
         <div class="flex flex-wrap justify-between">
-          {props.sourceList.map(source => {
+          <draggable v-model={dataList.value} item-key="key">
+            {{
+              item: ({ element }: any) => (
+                <div class="w-118px px-8px py-4px mb-8px border-1px border-solid border-gray-300 rounded-6px flex items-center cursor-pointer">
+                  <img src={getImgUrl(element.icon)} alt="" class="w-25px h-25px" />
+                  <div class="text-12px ml-4px">{element.name}</div>
+                </div>
+              ),
+            }}
+          </draggable>
+          {/* {props.sourceList.map(source => {
             return (
-              <div
-                draggable={true}
-                class="w-118px px-8px py-4px mb-8px border-1px border-solid border-gray-300 rounded-6px flex items-center cursor-pointer"
-              >
+              <div class="w-118px px-8px py-4px mb-8px border-1px border-solid border-gray-300 rounded-6px flex items-center cursor-pointer">
                 <img src={getImgUrl(source.icon)} alt="" class="w-25px h-25px" />
                 <div class="text-12px ml-4px">{source.name}</div>
               </div>
             );
-          })}
+          })} */}
         </div>
       );
     };
--- apps/FormDesign/src/components/Source/base.ts
+++ apps/FormDesign/src/components/Source/base.ts
@@ -1,13 +1,16 @@
 export default [
   {
+    key: 'input',
     name: '单行输入',
     icon: 'input.svg',
   },
   {
+    key: 'textArea',
     name: '多行输入',
     icon: 'textArea.svg',
   },
   {
+    key: 'select',
     name: '下拉选择',
     icon: 'select.svg',
   },
